a993338f5626b05bd844c975ccf421399a95b07e
DGUHJH <ghkdwogud852@gmail.com>
2021-06-17 15:37:59 +0900

    Add : styled-components

---
+++ src/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const BodyContainer = styled.div`
+	height: 100%;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
