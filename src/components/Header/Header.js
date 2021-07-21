7aaa315080835474e5d0eef6c1fa0dc93e9f7a52
DGUHJH <ghkdwogud852@gmail.com>
2021-07-21 10:18:08 +0900

    Update : Header Component Drawer, Menu OnClick

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,24 +2,43 @@ import * as Styled from './styled';
 import {
 	AppBar,
 	Button,
+	Drawer,
 	IconButton,
 	Toolbar,
 	Typography,
 } from '@material-ui/core';
 import { Menu } from '@material-ui/icons';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const App2 = () => {
+	const [drawerOpen, setDrawerOpen] = useState(false);
+
+	const handleOpen = (value) => () => {
+		setDrawerOpen(value);
+	};
+
 	return (
 		<Styled.Root>
 			<AppBar position="static">
 				<Toolbar>
-					<IconButton edge="start" color="inherit" aria-label="menu">
+					<IconButton
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						onClick={handleOpen(true)}
+					>
 						<Menu />
 					</IconButton>
 					<Typography variant="h6">🌼김소정을 소개합니다🌼</Typography>
 					<Button color="inherit">Login</Button>
 				</Toolbar>
 			</AppBar>
+			{/* Drawer 예제 */}
+			<Drawer anchor="left" open={drawerOpen} onClose={handleOpen(false)}>
+				{/* 페이지 이동 */}
+				<Link to="sub">서브 페이지</Link>
+			</Drawer>
 		</Styled.Root>
 	);
 };
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,5 +1,10 @@
+import { Typography } from '@material-ui/core';
 import styled from 'styled-components';
 
 export const Root = styled.div`
 	width: 100%;
+`;
+
+export const DrawerTypo = styled(Typography)`
+	font-family: NanumBarunGothic;
 `;
