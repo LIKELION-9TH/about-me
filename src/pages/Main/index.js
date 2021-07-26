787420fbf4c3e4895c2c763c08cee5543fbbad7c
DGUHJH <ghkdwogud852@gmail.com>
2021-07-26 10:13:29 +0900

    Add : Card, BootStrap

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import * as Styled from './styled';
 import Header from '../../components/Header/Header';
+import Card from '../../components/Card/Card';
 
 const App = () => {
 	return (
@@ -14,18 +15,15 @@ const App = () => {
 			<Styled.BasicInformationTitle>
 				👻김소정의 기본 정보👻
 			</Styled.BasicInformationTitle>
-			<Styled.BasicInformation>
-				이름 : 👩‍💻김 소 정👩‍💻
-			</Styled.BasicInformation>
+			<Styled.BasicInformation>이름 : 👩‍💻김 소 정👩‍💻</Styled.BasicInformation>
 			<Styled.BasicInformation>
 				생년월일 : 🎂1997.10.16🎂 - 현재 만 23세
 			</Styled.BasicInformation>
-			<Styled.BasicInformation>
-				MBTI : ⭐️ESFJ⭐️
-			</Styled.BasicInformation>
+			<Styled.BasicInformation>MBTI : ⭐️ESFJ⭐️</Styled.BasicInformation>
 			<Styled.BasicInformation>
 				학교 및 전공, 학년, 학적상태 : 🔮성신여자대학교 AI융합학부 1학년 재학🔮
 			</Styled.BasicInformation>
+			<Card />
 		</Styled.Root>
 	);
 };
