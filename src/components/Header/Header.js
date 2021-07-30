9c2019b12986ae5d7abc4500bc1397741205c4f3
DGUHJH <ghkdwogud852@gmail.com>
2021-07-30 10:23:47 +0900

    Update : Header Drawer Layout Width Size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,18 +21,24 @@ const App2 = () => {
 	return (
 		<Styled.Root>
 			<AppBar position="static">
-				<Toolbar style={{display: "flex", justifyContent: "space-between", alignItems: "center", }} >
-          <div style={{display: "flex", alignItems: "center"}}>
-					  <IconButton
-						  edge="start"
-						  color="inherit"
-						  aria-label="menu"
-						  onClick={handleOpen(true)}
-					  >
-						  <Menu />
-					  </IconButton>
-					  <Typography variant="h6">🌼김소정을 소개합니다🌼</Typography>
-          </div>
+				<Toolbar
+					style={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+					}}
+				>
+					<div style={{ display: 'flex', alignItems: 'center' }}>
+						<IconButton
+							edge="start"
+							color="inherit"
+							aria-label="menu"
+							onClick={handleOpen(true)}
+						>
+							<Menu />
+						</IconButton>
+						<Typography variant="h6">🌼김소정을 소개합니다🌼</Typography>
+					</div>
 					<Button color="inherit">Login</Button>
 				</Toolbar>
 			</AppBar>
@@ -40,20 +46,26 @@ const App2 = () => {
 			<Drawer anchor="left" open={drawerOpen} onClose={handleOpen(false)}>
 				{/* 페이지 이동 */}
 				<Styled.DrawerBody>
-          <div style={{fontSize: "25px", marginLeft: "40px"}}>
-            MENU
-          </div>
+					<div style={{ fontSize: '25px', marginLeft: '40px' }}>MENU</div>
 					<Styled.DrawerLink>
-            <button type="button" class="btn btn-outline-primary"><Link to="sub1">1) 🎵김소정이 좋아하는 음악🎵</Link></button>
+						<button type="button" class="btn btn-outline-primary">
+							<Link to="sub1">1) 🎵김소정이 좋아하는 음악🎵</Link>
+						</button>
 					</Styled.DrawerLink>
 					<Styled.DrawerLink>
-            <button type="button" class="btn btn-outline-success"><Link to="sub2">2) 🍯김소정이 좋아하는 맛집🍯</Link></button>
+						<button type="button" class="btn btn-outline-success">
+							<Link to="sub2">2) 🍯김소정이 좋아하는 맛집🍯</Link>
+						</button>
 					</Styled.DrawerLink>
 					<Styled.DrawerLink>
-            <button type="button" class="btn btn-outline-danger"><Link to="sub3">3) 🤸김소정이 좋아하는 취미🤸</Link></button>
+						<button type="button" class="btn btn-outline-danger">
+							<Link to="sub3">3) 🤸김소정이 좋아하는 취미🤸</Link>
+						</button>
 					</Styled.DrawerLink>
 					<Styled.DrawerLink>
-            <button type="button" class="btn btn-outline-info"><Link to="sub4">4) 📸김소정이 담겨있는 사진📸</Link></button>
+						<button type="button" class="btn btn-outline-info">
+							<Link to="sub4">4) 📸김소정이 담겨있는 사진📸</Link>
+						</button>
 					</Styled.DrawerLink>
 				</Styled.DrawerBody>
 			</Drawer>
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,4 +1,4 @@
-import { Typography,Drawer } from '@material-ui/core';
+import { Typography, Drawer } from '@material-ui/core';
 import styled from 'styled-components';
 
 export const Root = styled.div`
@@ -9,18 +9,17 @@ export const DrawerTypo = styled(Typography)`
 	font-family: NanumBarunGothic;
 `;
 
-
 export const DrawerBody = styled.div`
-&&&{
-	width: 20vw;
-	display: flex;
-	flex-direction: column;
-	padding-top: 50px;
-}
+	&&& {
+		width: 300px;
+		display: flex;
+		flex-direction: column;
+		padding-top: 50px;
+	}
 `;
 
 export const DrawerLink = styled.div`
 	margin-top: 20px;
 	margin-left: 30px;
 	font-size: 20px;
-`
+`;
